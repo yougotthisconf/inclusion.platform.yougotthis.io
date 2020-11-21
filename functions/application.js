df37d7530d95ccdc8bd78fdf80eb926660e5645d
Kevin Lewis <kevin.lewis@vonage.com>
2020-11-21 16:12:31 +0000

    Ready to deploy

--- functions/application.js
+++ functions/application.js
@@ -1,6 +1,18 @@
+const { AIRTABLE_KEY, AIRTABLE_BASE, AIRTABLE_TABLE } = process.env
+const Airtable = require('airtable-plus');
+
+const airtable = new Airtable({
+    baseID: AIRTABLE_BASE,
+    apiKey: AIRTABLE_KEY,
+    tableName: AIRTABLE_TABLE
+})
+
 exports.handler = async event => {
   try {
-    return res({ message: 'ok' })
+    const { fields: application } = await airtable.find(event.queryStringParameters.id)
+    const accepted = Boolean(application.Status && application.Status == 'Accepted')
+
+    return res({ accepted })
   } catch (error) {
     return res({ error }, 500)
   }
